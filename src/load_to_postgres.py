13f233bf12615d48a082143c6eef36423f9cba36
Bitania Finoteselam <hanafinoteselam.15@gmail.com>
2026-06-28 20:04:59 +0300

    Add PostgreSQL connection and create table script

--- src/load_to_postgres.py
+++ src/load_to_postgres.py
@@ -1,1 +1,26 @@
 
+import json
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="medical",
+    user="postgres",
+    password="password"
+)
+
+cur = conn.cursor()
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS telegram_messages(
+    message_id INTEGER,
+    message_text TEXT,
+    views INTEGER,
+    forwards INTEGER
+)
+""")
+
+conn.commit()
+
+cur.close()
+conn.close()
